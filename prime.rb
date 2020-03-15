7dc57a216fa2f5ec0a11c3e0367a2a76e3b60750
Doug Jones <dsjr1284@gmail.com>
2020-03-15 19:25:28 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,17 @@
 # Prime number does not have a remainder when divided by itself 
+# if any number is less than or = to 1 its not a prime #
+  # retrun false 
+  
+  # Check all numbers inside on int greater than with no 1 remainder 
+  # if there are any numbers with a remainder = to 0 
+  # returns true. 
+  
+  # if we divided a number by 
 
 def prime?(int)
-  i = 2 
-  if i < 2 
+ 
   
-
+end 
 
 
 
