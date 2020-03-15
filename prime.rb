4cf04e0bc8c294e7657b64c1c82d60adfe221223
Doug Jones <dsjr1284@gmail.com>
2020-03-15 19:29:21 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
-# Prime number does not have a remainder when divided by itself 
+# Prime number if we have a remainder from divding by 2 
 # if any number is less than or = to 1 its not a prime #
   # retrun false 
   
   # Check all numbers inside on int greater than with no 1 remainder 
   # if there are any numbers with a remainder = to 0 
-  # returns true. 
+  # returns false. 
   
   # if we divided a number by 
 
