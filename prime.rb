e202d697b7308425b8ab7b6acf69435cf935dbc0
Doug Jones <dsjr1284@gmail.com>
2020-03-15 19:59:18 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,34 @@
-# Add  code here!
+require "pry"
+
+# Prime number if we have a remainder from divding by 2 
+# if any number is less than or = to 1 its not a prime #
+  # retrun false 
+  
+  # Check all numbers inside on int greater than with no 1 remainder 
+  # if there are any numbers with a remainder = to 0 
+  # returns false. 
+  
+  # Anything else return true. 
+
+def prime?(int)
+ if int <= 1 
+   return false
+ else 
+   if (2...int).any? {|n| int % n == 0}
+     return false 
+   else 
+     return true 
+    end 
+  end 
+  binding.pry 
+end 
+
+
+
+
+
+
+
+
+
+
