905e50aede0cd38655a6ebc3d8eeee7cb8c935b2
Doug Jones <dsjr1284@gmail.com>
2020-03-15 19:56:35 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,4 @@
-# requrie "pry"
+requrie "pry"
 
 # Prime number if we have a remainder from divding by 2 
 # if any number is less than or = to 1 its not a prime #
@@ -13,12 +13,14 @@
 def prime?(int)
  if int <= 1 
    return false
+   binding.pry 
  else 
    if (2...int).any? {|n| int % n == 0}
      return false 
    else 
      return true 
     end 
+    
   end 
 end 
 
